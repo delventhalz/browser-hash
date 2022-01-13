607e3b9b5e49c310befa866bdaf49f16f1ad56e0
Zac Delventhal <delventhalz@gmail.com>
2022-01-13 09:26:37 -0600

    Add TypeScript declarations
    Who doesn't love types?

---
+++ browser-hash.d.ts
@@ -0,0 +1,11 @@
+export enum BrowserHashString {
+  'SHA-1',
+  'SHA-256',
+  'SHA-384',
+  'SHA-512'
+}
+export function listKeys(val: unknown): string[];
+export function toSortedJson(val: unknown): string;
+export function toBytes(val: unknown): Uint8Array;
+export function bufferToHex(buff: ArrayBuffer): string;
+export function browserHash(val: unknown, algo?: BrowserHashString): string;
