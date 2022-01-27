4626ff43805eb05d1bba94c2f0c9748262491ea3
Zac Delventhal <delventhalz@gmail.com>
2022-01-27 13:21:15 -0600

    Fix buffer type declarations
    ArrayBuffer does not cover ArrayBufferViews. Most ArrayBufferViews are
    TypedArrays, but that class is not exposed globally.

--- browser-hash.d.ts
+++ browser-hash.d.ts
@@ -1,5 +1,22 @@
-export function isBuffer(val: unknown): val is ArrayBuffer;
+// The TypedArray class is not exposed globally
+export type TypedArray = Int8Array
+    | Uint8Array
+    | Uint8ClampedArray
+    | Int16Array
+    | Uint16Array
+    | Int32Array
+    | Uint32Array
+    | Float32Array
+    | Float64Array
+    | BigInt64Array
+    | BigUint64Array;
+
+export type BrowserBuffer = ArrayBuffer
+    | TypedArray
+    | DataView;
+
+export function isBuffer(val: unknown): val is BrowserBuffer;
 export function stringToBuffer(str: string): Uint8Array;
-export function bufferToHex(buffer: ArrayBuffer): string;
-export function bufferHash(val: string|ArrayBuffer, algo?: string): Uint8Array;
-export function browserHash(val: string|ArrayBuffer, algo?: string): string;
+export function bufferToHex(buffer: BrowserBuffer): string;
+export function bufferHash(strOrBuffer: string|BrowserBuffer, algo?: string): Uint8Array;
+export function browserHash(strOrBuffer: string|BrowserBuffer, algo?: string): string;
